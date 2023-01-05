d93a12fc39ab606c68e4bf20ab66d6eb61831763
suryadev008 <suraj@ditinteractive.com>
2023-01-05 12:15:43 +0530

    add from work

--- src/services/todo.js
+++ src/services/todo.js
@@ -17,55 +17,74 @@ async function listTodos(req,res,next) {
 // TODO: list all todo data
 async function createTodo(req,res,next) {
     let postData=req.body;
-
     let data={
         "title":(postData.title?postData.title:null),
         "todo_desc":(postData.todo_desc?postData.todo_desc:null),
         "date":(postData.date?postData.date:null)
     }
-    await knex(table).insert(data).then((result)=>{
+    await knex(table).insert(data).then(async (result)=>{
        if(result){
+        let data=await knex(table).where('id',result)
         res.status(200).json({
             error: false,
-            message: "Data inserted",
-            "rowid":result
+            message: "Data Inserted",
+            data
           });
+       }else{
+        res.status(409).json({
+          error: true,
+          message: "Data Not Inserted"
+        });
        }
     })  
 }
-
 
 // TODO: get data from db for a single row
 async function editTodo(req,res,next) {
     console.log(req.body)
     let id=req.body.id
     console.log(id)
-
-    let data=await knex(table).where('id',id)
-    res.status(200).json({
-        error: false,
-        message: "success",
-        data
-      });
+    if(id>0){
+      let data=await knex(table).where('id',id)
+      res.status(200).json({
+          error: false,
+          message: "success",
+          data
+        });
+    }else{
+     
+    }
+  
 }
 
 
 // TODO: update data from single row
 async function updateTodo(req,res,next) {
-    console.log(req.body)
+    // console.log(req.body)
     let id=req.body.id
     delete req.body.id
-    console.log(id)
-    console.log(req.body)
+    // console.log(id)
+    // console.log(req.body)
     let updateData=req.body;
 
     let data=await knex(table)
     .where('id',id)
-    .update(updateData);
-    res.status(200).json({
-        error: false,
-        message: "update success"
-      });
+    .update(updateData).then(async (result)=>{
+      console.log(result)
+      if(result==1){
+       let data=await knex(table).where('id',id)
+       res.status(200).json({
+           error: false,
+           message: "Data Update",
+           data
+         });
+      }else{
+       res.status(409).json({
+         error: true,
+         message: "Data Not Update"
+       });
+      }
+   });
 }
 
 // TODO: update data from single row
